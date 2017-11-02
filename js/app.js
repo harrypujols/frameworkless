34f6f0bce5caee489378e6f76dedb154563c8e3c
Harry Pujols <harrypujols@me.com>
2017-11-02 13:44:24 -0400

    New loop execution

--- js/app.js
+++ js/app.js
@@ -70,121 +70,46 @@
 "use strict";
 
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 (function () {
   var APP = APP || {};
 
-  APP.component = {
-    js: function js(el) {
-      return document.querySelectorAll('[data-js=' + el + ']');
+  APP.helpers = {};
+
+  APP.components = {
+    js: function js() {
+      var components = document.querySelectorAll('[data-js]');
+      components.forEach(function (component) {
+
+        Object.entries(APP.methods).forEach(function (entry) {
+          var key = entry[0];
+          var value = entry[1];
+
+          if (key == component.dataset.js) {
+            var method = new value();
+            console.log(method);
+          }
+        });
+      });
     }
   };
 
   APP.methods = {
-    carousel: function () {
-      function carousel() {
-        _classCallCheck(this, carousel);
+    carousel: function carousel() {
+      _classCallCheck(this, carousel);
+    },
 
-        this.carousel = APP.component.js('carousel');
-        this.right;
-        this.left;
-        this.items;
-        this.index = 0;
-      }
+    svg: function svg() {
+      _classCallCheck(this, svg);
+    }
 
-      _createClass(carousel, [{
-        key: 'next',
-        value: function next() {
-          var _this = this;
+    // Object.values( APP.methods ).forEach(( method ) => {
+    //   let execute = new method
+    //   execute.init()
+    // })
 
-          this.right.addEventListener('click', function (event) {
-            event.preventDefault();
-            _this.index++;
-
-            if (_this.index >= _this.items.length) {
-              _this.index = _this.items.length - 1;
-            }
-
-            console.log(_this.index);
-
-            _this.items[_this.index].scrollIntoView({ block: 'end', behaviour: 'smooth' });
-          });
-        }
-      }, {
-        key: 'prev',
-        value: function prev() {
-          var _this2 = this;
-
-          this.left.addEventListener('click', function (event) {
-            event.preventDefault();
-            _this2.index--;
-            if (_this2.index < 0) {
-              _this2.index = 0;
-            }
-            _this2.items[_this2.index].scrollIntoView({ block: 'end', behaviour: 'smooth' });
-          });
-        }
-      }, {
-        key: 'init',
-        value: function init() {
-          var _this3 = this;
-
-          this.carousel.forEach(function (carousel) {
-            _this3.right = carousel.querySelector('[data-btn=next]');
-            _this3.left = carousel.querySelector('[data-btn=prev]');
-            _this3.items = carousel.querySelectorAll('.carousel-item');
-            _this3.next();
-            _this3.prev();
-          });
-        }
-      }]);
-
-      return carousel;
-    }(),
-
-    svg: function () {
-      function svg() {
-        _classCallCheck(this, svg);
-
-        this.element = document.querySelectorAll('[data-svg]');
-      }
-
-      _createClass(svg, [{
-        key: 'importer',
-        value: function importer() {
-          var _this4 = this;
-
-          this.element.forEach(function (element) {
-            _this4.fetcher(element, element.dataset.svg);
-          });
-        }
-      }, {
-        key: 'fetcher',
-        value: function fetcher(element, file) {
-          fetch(file).then(function (response) {
-            return response.text();
-          }).then(function (text) {
-            element.innerHTML = text;
-          }).catch(console.error.bind(console));
-        }
-      }, {
-        key: 'init',
-        value: function init() {
-          this.importer();
-        }
-      }]);
-
-      return svg;
-    }()
-  };
-
-  Object.values(APP.methods).forEach(function (method) {
-    var execute = new method();
-    execute.init();
-  });
+  };APP.components.js();
 })();
 
 /***/ })
