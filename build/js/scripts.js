d9ba6b9b66e341712b3466f2b4ec0b94a1b9a24c
Harry Pujols <harry@unqork.com>
2021-06-21 17:43:57 -0400

    changing the task runner slide

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -430,7 +430,7 @@ __webpack_require__.r(__webpack_exports__);
 __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = ({
   data : [
-    { title: 'Gulp JS', text: 'As principal task runner, with browser sync.'},
+    { title: 'NPM Scripts', text: 'As principal task runner.'},
     { title: 'Webpack', text: 'With Babel, as ES6 transpiler.'},
     { title: 'Sass', text: 'As the css pre-processor.'},
     { title: 'No libraries', text: 'No frameworks, just vanilla JS.'}
--- dev/js/templates/slides.js
+++ dev/js/templates/slides.js
@@ -1,6 +1,6 @@
 export default {
   data : [
-    { title: 'Gulp JS', text: 'As principal task runner, with browser sync.'},
+    { title: 'NPM Scripts', text: 'As principal task runner.'},
     { title: 'Webpack', text: 'With Babel, as ES6 transpiler.'},
     { title: 'Sass', text: 'As the css pre-processor.'},
     { title: 'No libraries', text: 'No frameworks, just vanilla JS.'}
